c2fb769e192d72f2c52cfe959e44a76740dcd6f2
amirsouayah <amirsouayah@gmail.com>
2023-04-22 16:57:17 +0200

    formulaire de test

--- components/InfoCard.js
+++ components/InfoCard.js
@@ -14,6 +14,7 @@ function InfoCard({ img, location, title, description, star, price, total }) {
                     layout='fill'
                     objectFit="cover"
                     className='rounded-2xl'
+                    alt=''
                 />
             </div>
             <div className='flex flex-col flex-grow pl-5'>
